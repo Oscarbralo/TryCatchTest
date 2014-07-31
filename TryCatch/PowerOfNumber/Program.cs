293c8e8f2f16598e5f16032f2f565c58fe0a38a6
U-Alethor-PC\Alethor <Alethor@Alethor-PC.(none)>
2014-07-31 20:07:30 +0200

    new change to not use Math.Pow()

--- TryCatch/PowerOfNumber/Program.cs
+++ TryCatch/PowerOfNumber/Program.cs
@@ -8,18 +8,32 @@ namespace PowerOfNumber
         {
             try
             {
+                Pow pow = new Pow();
                 Console.WriteLine("Please enter the x number:");
                 long number = long.Parse(Console.ReadLine());
                 Console.WriteLine("Please enter the y index: ");
                 long index = long.Parse(Console.ReadLine());
                 Console.WriteLine("The result is: ");
-                Console.WriteLine(Math.Pow(number, index));
+                Console.WriteLine(pow.getPow(number, index));
             }
             catch
             {
                 Console.WriteLine("Need to enter numbers in the long range");
             }
             Console.ReadLine();
+        }
+    }
+
+    class Pow
+    {
+        public long getPow(long n, long index)
+        {
+            long result = n;
+            for (long i = 0; i < index; i++)
+            {
+                result *= n;
+            }
+            return result;
         }
     }
 }
